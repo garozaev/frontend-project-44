4fdbc4264a69de682ddaa4cd117e20c463c9e0e8
Georgy Rozaev <g.a.rozaev@gmail.com>
2024-09-29 18:31:00 +0300

    update index.js !

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const gameEngine = (user, condition, round, question, check, finish) => {
     if (round === finish - 1) {
         console.log(`Question: ${question} `);
         const playerAnswer = readlineSync.question('Yuor answer: ');
-        answer = check === playerAnswer ? 'Correct!' : `'${playerAnswer}' is wrong answer ;(. Correct answer was '${check}'.\nLet's try again, ${user} `;
+        answer = check === playerAnswer ? 'Correct!' : `'${playerAnswer}' is wrong answer ;(. Correct answer was '${check}'.\nLet's try again, ${user}!`;
         if (answer === 'Correct!') {
             answer = `Correct!\nCongratulations, ${user}!`;
         }
